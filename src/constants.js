b567b0b6b13d8ec55d5a9912225a60bec039d360
vidhya-metacell <71261494+vidhya-metacell@users.noreply.github.com>
2021-07-02 16:51:47 +0530

    #43 File extension update

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const IMPORT_TEXT = 'Import a new dataset';
 export const FILE_UPLOAD_PARAMS = {
-  acceptedFileExtensions: ['.json', '.mtl'],
+  acceptedFileExtensions: ['.json', '.ttl'],
   maxFileSize: 5242880
 };
