ed8038e1dd2c6f0677a868b2321dfdb66cd57854
vidhya-metacell <71261494+vidhya-metacell@users.noreply.github.com>
2021-07-02 21:11:28 +0530

    #45 Node detail drawe

---
+++ src/components/NodeDetailView/NodeDetailView.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Box } from "@material-ui/core";
+import NodeHeader from "./Header/Header";
+import Details from "./Details/Details";
+import NodeFooter from "./Footer";
+import { NODES } from '../../constants';
+
+const NodeDetailView = (props) => {
+  const nodeHeading = NODES[1];
+  // available options to render different views - 'matlab', 'nifti', 'volume' just change the NODES index for now
+
+  const headerLinks = {
+    pages: [
+      {
+        title: '...',
+        href: '/'
+      },
+      {
+        title: 'Volume',
+        href: '/'
+      },
+    ],
+    current: nodeHeading
+  };
+
+  return (
+    <Box className={"secondary-sidebar" + (props.open ? " in" : "")}>
+      <NodeHeader close={props.handleClose} links={headerLinks} heading={nodeHeading} />
+      <Details close={props.handleClose} nodeHeading={nodeHeading} />
+      <NodeFooter />
+    </Box>
+  );
+};
+export default NodeDetailView;
+
