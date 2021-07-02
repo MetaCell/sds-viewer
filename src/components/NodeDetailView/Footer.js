cb0dce1bc5ee3b14badaa872e8db1fa722520a86
vidhya-metacell <71261494+vidhya-metacell@users.noreply.github.com>
2021-07-02 21:13:00 +0530

    #45 Header, Footer node detai drawer view

---
+++ src/components/NodeDetailView/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box } from '@material-ui/core';
+import FILTER from '../../images/filter.svg';
+
+const NodeFooter = () => {
+	return (
+		<Box className='secondary-sidebar_footer'>
+			<img src={FILTER} alt='FILTER' />
+		</Box>
+	);
+};
+
+export default NodeFooter;
