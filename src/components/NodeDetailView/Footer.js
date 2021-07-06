fe03d6d353a114a38270631da2f33d1776c3899d
vidhya-metacell <71261494+vidhya-metacell@users.noreply.github.com>
2021-07-06 16:49:04 +0530

    #45 Indentation fix

--- src/components/NodeDetailView/Footer.js
+++ src/components/NodeDetailView/Footer.js
@@ -3,11 +3,11 @@ import { Box } from '@material-ui/core';
 import FILTER from '../../images/filter.svg';
 
 const NodeFooter = () => {
-	return (
-		<Box className='secondary-sidebar_footer'>
-			<img src={FILTER} alt='FILTER' />
-		</Box>
-	);
+  return (
+    <Box className='secondary-sidebar_footer'>
+      <img src={FILTER} alt='FILTER' />
+    </Box>
+  );
 };
 
 export default NodeFooter;
