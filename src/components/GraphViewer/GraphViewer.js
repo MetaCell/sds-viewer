035c570074f3da3bb09144bd915b5205af6f4b88
vidhya-metacell <71261494+vidhya-metacell@users.noreply.github.com>
2021-07-27 20:34:21 +0530

    #52 Nodes and links graph styling

--- src/components/GraphViewer/GraphViewer.js
+++ src/components/GraphViewer/GraphViewer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IconButton } from '@material-ui/core';
 import ZoomOutIcon from '@material-ui/icons/ZoomOut';
 import ZoomInIcon from '@material-ui/icons/ZoomIn';
@@ -6,7 +6,7 @@ import RefreshIcon from '@material-ui/icons/Refresh';
 import LayersIcon from '@material-ui/icons/Layers';
 import GeppettoGraphVisualization from '@metacell/geppetto-meta-ui/graph-visualization/Graph';
 
-const NODE_FONT = "8px sans-serif";
+const NODE_FONT = "6px sans-serif";
 const ONE_SECOND = 1000;
 const ZOOM_DEFAULT = 1;
 const ZOOM_SENSITIVITY = .2;
@@ -45,6 +45,49 @@ const GraphViewer = (props) => {
     setTimeout( () => graphRef?.current?.ggv?.current?.zoomToFit(), ONE_SECOND/2);
   });
 
+  const [highlightNodes, setHighlightNodes] = useState(new Set());
+  const [hoverNode, setHoverNode] = useState(null);
+
+  const updateHighlight = () => {
+    setHighlightNodes(highlightNodes);
+  };
+
+  const handleNodeHover = node => {
+    highlightNodes.clear();
+    if (node) {
+      highlightNodes.add(node);
+    }
+    setHoverNode(node || null);
+    updateHighlight();
+  };
+
+  const paintRing = React.useCallback((node, ctx) => {
+    const size = 20;
+    ctx.beginPath();
+    ctx.drawImage(node.img, node.x - size, node.y - size , size * 2, size * 2);
+
+    ctx.font = NODE_FONT;
+    ctx.textAlign = "center";
+    ctx.textBaseline = 'middle';
+    // Create Title in Node
+    ctx.fillText(node.name, node.x, node.y + 11 + size);
+    if(node === hoverNode){
+      ctx.globalAlpha = 0.5;
+      ctx.fillStyle = '#CFD4DA';
+      ctx.fillRect(node.x - size, node.y - size , size * 2, size * 2);
+      ctx.globalAlpha = 1.0;
+      ctx.fillStyle = '#3779E1';
+      ctx.fillRect(node.x - 15, node.y + size + 5, size * 1.5, 12 );
+      ctx.fillStyle = "white";
+      ctx.fillText(node.name, node.x, node.y + 11 + size);
+    } else {
+      ctx.fillStyle = "black";
+      ctx.fillText(node.name, node.x, node.y + 11 + size);
+    }
+
+    node.fy = 100 * node.level;
+  }, [hoverNode]);
+
   return (
     <div className={"graph-view"}>
       <GeppettoGraphVisualization
@@ -56,22 +99,15 @@ const GraphViewer = (props) => {
         nodeRelSize={20}
         nodeSize={30}
         // Links properties
-        linkColor="black"
-        linkCurvature={ .3 }
+        linkColor="#2E3A59"
+        linkCurvature={link => link.target.x < link.source.x ? -.2 : .2}
+        // Allows updating link properties, as color and curvature. Without this, linkCurvature doesn't work.
+        linkCanvasObjectMode={"replace"}
         onNodeClick = { (node,event) => handleNodeClick(node,event) }
+        onNodeHover={handleNodeHover}
         // Override drawing of canvas objects, draw an image as a node
-        nodeCanvasObject={(node, ctx, globalScale) => {
-          const size = 12;
-          ctx.drawImage(node.img, node.x - size, node.y - size , size *2, size * 2);
-
-          ctx.font = NODE_FONT;
-          ctx.textAlign = "center";
-          ctx.textBaseline = 'middle';
-          // Create Title in Node
-          ctx.fillText(node.name,node.x, node.y - size);
-
-          node.fy = 100 * node.level;
-        }}
+        nodeCanvasObject={paintRing}
+        // nodeCanvasObjectMode={node => highlightNodes.has(node) ? 'before' : undefined}
         // td = Top Down, creates Graph with root at top
         dagMode="td"
         // Handles error on graph
