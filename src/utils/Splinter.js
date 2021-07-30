7c8e5b2b2622ac98683262ec53b6958f1fd972f9
vidhya-metacell <71261494+vidhya-metacell@users.noreply.github.com>
2021-07-30 19:18:50 +0530

    Feature/52 Graph nodes styling (#65)
    * #52 Nodes and links graph styling
    * #52 New images for the graph, fix sizes on the canvas
    * #52 Graph viewer UI
    * #52 UI graphviewer
    * #52 Resize the node image
    * #52 Decrease distance between the targets from same source, also line curve

--- src/utils/Splinter.js
+++ src/utils/Splinter.js
@@ -14,33 +14,33 @@ class Splinter {
         this.dataset_id = this.processDatasetId();
         this.store = new N3.Store();
         this.graphRoot = undefined;
-        const imgs = ['dataset.svg', 'nifti.svg', 'volume.svg'].map(src => {
+        const imgs = ['database.svg', 'nft.svg', 'mtl.svg', 'folder.svg'].map(src => {
             const img = new Image();
-            img.src = `./images/${src}`;
+            img.src = `./images/graph/${src}`;
             console.log("Image source ", img.src);
             return img;
         });
         this.graph = {
             nodes: [
-                { id: 1 , name : "Dataset", img : imgs[0], level : 1 },
-                { id: 2 , name : " Subject 2", img : imgs[1], level : 2 },
-                { id: 3 , name : "Subject 3", img : imgs[1], level : 2 },
-                { id: 4 , name : "Subject 4", img : imgs[1], level : 3 },
-                { id: 5 , name : "Subject 5", img : imgs[1], level : 3 },
-                { id: 6 , name : "File 1", img : imgs[1], level : 3 },
-                { id: 7 , name : "File 2", img : imgs[1], level : 3 },
-                { id: 8 , name : "File 3", img : imgs[1], level : 4 },
-                { id: 9 , name : "File 4", img : imgs[1], level : 4 }
+                { id: 1 , name : "Dataset 106", img : imgs[0], level : 1 },
+                { id: 2 , name : "Volume", img : imgs[3], level : 2 },
+                { id: 3 , name : "Motility", img : imgs[3], level : 2 },
+                { id: 4 , name : "Volume", img : imgs[3], level : 3 },
+                { id: 5 , name : "Matlab", img : imgs[2], level : 3 },
+                { id: 6 , name : "Nifti", img : imgs[1], level : 3 },
+                { id: 7 , name : "Matlab", img : imgs[2], level : 3 },
+                { id: 8 , name : "Matlab", img : imgs[1], level : 4 },
+                { id: 9 , name : "Nifti", img : imgs[2], level : 4 }
             ],
             links: [
                 { source: 1, target: 2},
                 { source: 1, target: 3},
                 { source: 2, target: 4},
                 { source: 2, target: 5},
+                { source: 3, target: 6},
+                { source: 2, target: 7},
                 { source: 4, target: 8},
                 { source: 4, target: 9},
-                { source: 3, target: 6},
-                { source: 3, target: 7}
             ]
         };
         this.tree = {
