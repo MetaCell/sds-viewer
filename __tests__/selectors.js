fe79ed4f30749f074349420d570281d148ff32f4
Simão sá <simao@metacell.us>
2023-02-27 10:49:37 +0000

    initial set up for puppeteer tests

---
+++ __tests__/selectors.js
@@ -0,0 +1,22 @@
+//Selectors: 
+
+export const EMPTY_DATASET_VIEWER_SELECTOR = '.MuiBox-empty';
+export const EMPTY_DATASET_LIST_SELECTOR = '.no-instance';
+export const LOAD_BUTTONS_SELECTOR = 'button[class="MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-disableElevation"]';
+export const DATASET_LIST_SELECTOR = '.datasets_list';
+export const DATASET_ITEM_SELECTOR = '.dataset_item';
+export const DONE_BUTTON_SELECTOR = 'button[class="MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-disableElevation Mui-disabled MuiButton-fullWidth Mui-disabled"]';
+export const DATASET_COUNT_NUMBER_SELECTOR = 'p[class="MuiTypography-root MuiTypography-body1"]';
+export const SPARC_DATASET_LIST_SELECTOR = 'li[class="MuiListItem-root dataset_item MuiListItem-gutters"]';
+export const ENABLED_DONE_BUTTON_SELECTOR = 'button[class="MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-disableElevation MuiButton-fullWidth"]';
+export const LOADED_DATASET_SELECTOR = 'p[class="MuiTypography-root labelText MuiTypography-body2"]';
+export const GRAPH_SELECTOR = '#graph-2d';
+export const OPEN_FOLDER_BUTTON_SELECTOR = 'span[class="MuiTypography-root labelCaption MuiTypography-caption MuiTypography-colorInherit"]';
+export const FELINE_DATASET_IMG_SELECTOR = 'img[alt="Feline brainstem neuron extracellular potential recordings"]';
+export const DATA_ID_SELECTOR = 'h6[class="MuiTypography-root breadcrumb_selected MuiTypography-subtitle2"]';
+export const URL_UPLOADER_SELECTOR = '#url-uploader';
+export const LOAD_DATA_POPUP_SELECTOR = 'div[class="MuiTabs-root rounded"]';
+export const LOAD_THROUGH_URL_TAB_SELECTOR = '#simple-tab-1';
+export const LOCAL_UPLOAD_TAB_SELECTOR = '#simple-tab-0';
+export const LOCAL_SYSTEM_UPLOAD_BUTTON_SELECTOR = '#simple-tabpanel-0';
+
---
+++ jest-puppeteer.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    launch: {
+      headless: true,
+      // slowMo:20,
+      defaultViewport: {
+        width: 1000,
+        height: 800
+      },
+      timeout: 120000
+    },
+  }
