a0a1f2766a26e1bbd6259d1be4df6201d24b813c
Simão sá <simao@metacell.us>
2023-02-27 10:54:51 +0000

    step for opening the dataset list

--- __tests__/e2e.test.js
+++ __tests__/e2e.test.js
@@ -1,6 +1,6 @@
 import * as puppeteer from "puppeteer";
 import * as selectors from "./selectors";
-import { ONE_SECOND, ONE_MINUTE, TWO_MINUTES, TEN_MINUTES} from "./time_constants";
+import { ONE_SECOND, ONE_MINUTE, TWO_MINUTES, TEN_MINUTES } from "./time_constants";
 import 'expect-puppeteer';
 import 'puppeteer'
 import { toMatchImageSnapshot } from 'jest-image-snapshot'
@@ -56,7 +56,40 @@ describe("SDS Viewer e2e Test: Sparc Dataset", () => {
                 customSnapshotIdentifier: 'Home Page'
             });
 
-
     });
+
+
+    test('Choose from SPARC Dataset List', async () => {
+
+        console.log('Opening SPARC Datasets')
+
+        await page.waitForSelector(selectors.LOAD_BUTTONS_SELECTOR)
+
+        const load_dataset_button = await page.$$(selectors.LOAD_BUTTONS_SELECTOR)
+
+        for (var i = 0; i < load_dataset_button.length; i++) {
+            load_dataset_button[1].click()
+
+        }
+
+        await page.waitForSelector(selectors.DATASET_LIST_SELECTOR)
+        await page.waitForSelector(selectors.DATASET_ITEM_SELECTOR)
+        await page.waitForSelector(selectors.DONE_BUTTON_SELECTOR, { disabled: true })
+        const sparc_dataset_counts = await page.$$eval(selectors.DATASET_COUNT_NUMBER_SELECTOR, sparc_dataset_counts => {
+            return sparc_dataset_counts.map(sparc_dataset_count => sparc_dataset_count.innerHTML);
+        });
+        const sparc_dataset_list = await page.$$(selectors.SPARC_DATASET_LIST_SELECTOR)
+        const sparc_dataset_list_count = sparc_dataset_list.length
+        const sparc_dataset_list_count_string = sparc_dataset_list_count.toString();
+
+        expect(sparc_dataset_counts[0]).toContain(sparc_dataset_list_count_string)
+
+        for (var i = 0; i < sparc_dataset_list_count; i++) {
+            await sparc_dataset_list[0].click()
+        }
+
+        await page.waitForSelector(selectors.ENABLED_DONE_BUTTON_SELECTOR)
+
+    })
 
 })
