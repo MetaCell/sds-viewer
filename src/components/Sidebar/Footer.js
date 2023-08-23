b08a72bf281c91d68456abf02047d52a3de43ba3
jrmartin <yeshuarmartinez@gmail.com>
2023-08-23 13:12:20 -0700

    Add link for user manual

--- src/components/Sidebar/Footer.js
+++ src/components/Sidebar/Footer.js
@@ -1,30 +1,35 @@
 import Plus from '../../images/plus.svg';
 import { ADD_DATASET } from '../../constants';
-import { Box, Button } from '@material-ui/core';
+import { Box, Button, Typography } from '@material-ui/core';
 import config from "./../../config/app.json";
 
 const SidebarFooter = (props) => {
   return (
-    <Box display="flex" justifyContent="space-between">
-      <Button
-        variant='contained'
-        disableElevation
-        color='primary'
-        style={{ "width" : "45%","minWidth" : "45%" }}
-        onClick={() => props.setOpenUploadDialog(true)}
-      >
-        <img src={Plus} alt='Plus' />
-        {ADD_DATASET}
-      </Button>
-      <Button
-        variant='contained'
-        disableElevation
-        color='primary'
-        style={{ "width" : "45%", "minWidth" : "45%" }}
-        onClick={() => props.setOpenDatasetsListDialog(true)}
-      >
-        {config.text.datasetsButtonText}
-      </Button>
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Button
+          variant='contained'
+          disableElevation
+          color='primary'
+          style={{ "width" : "45%","minWidth" : "45%" }}
+          onClick={() => props.setOpenUploadDialog(true)}
+        >
+          <img src={Plus} alt='Plus' />
+          {ADD_DATASET}
+        </Button>
+        <Button
+          variant='contained'
+          disableElevation
+          color='primary'
+          style={{ "width" : "45%", "minWidth" : "45%" }}
+          onClick={() => props.setOpenDatasetsListDialog(true)}
+        >
+          {config.text.datasetsButtonText}
+        </Button>
+      </Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography align="center" className='no-instance' variant='h6'><a style={{"color" : "white" }} align="center" href={config.docs_url} target="_blank">User manual</a></Typography>
+      </Box>
     </Box>
   );
 };
