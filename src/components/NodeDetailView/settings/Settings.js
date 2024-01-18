a765626a64cc919bd554f31a7f80898c16e8fb4c
salam dalloul <salamdalloul93@gmail.com>
2024-01-18 20:27:29 +0100

    remove unnecessary code

--- src/components/NodeDetailView/settings/Settings.js
+++ src/components/NodeDetailView/settings/Settings.js
@@ -17,22 +17,6 @@ const Settings = props => {
   };
   return (
     <Box style={{ position: "relative", maxHeight: "84vh", overflow: "auto" }}>
-      {/*<Box*/}
-      {/*  style={{*/}
-      {/*    display: "flex",*/}
-      {/*    justifyContent: "center",*/}
-      {/*    alignItems: "center",*/}
-      {/*    flexDirection: "column",*/}
-      {/*    padding: "0rem 1.5rem 2.25rem 1.5rem",*/}
-      {/*    boxShadow: "0px 2px 0px 0px #E5E5E5",*/}
-      {/*    gap: ".75rem"*/}
-      {/*  }}*/}
-      {/*>*/}
-      {/*  <FolderIcon style={{ color: "#3779E1" }} />*/}
-      {/*  <Typography variant="h3" style={{ color: "#3779E1" }}>*/}
-      {/*    First List Title*/}
-      {/*  </Typography>*/}
-      {/*</Box>*/}
         {
             Object.keys(metaDataPropertiesModel).map(group => <SettingsGroup title={group} group={metaDataPropertiesModel[group]} />)
         }
